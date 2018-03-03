dde32d23aaa483399edb666a80aebe050e9c6176
jllan <cbj_love@126.com>
2018-03-03 18:13:45 +0800

    改进代理程序，添加了“66ip”和“goubanjia”两个源

--- proxy_pool/proxy.py
+++ proxy_pool/proxy.py
@@ -1,5 +1,5 @@
 import random
-import json
+import re
 import time
 import requests
 from lxml import etree
@@ -16,60 +16,147 @@ class ProxyPool():
     def __init__(self):
         pass
 
-    def get_xici(self, url):
-        """
-        抓取xici代理
-        :param url:
-        :return:
-        """
+    def get_xici(self):
+        """抓取xici代理"""
+        print('从xici抓取代理')
+        results = set()
+        url = 'http://www.xicidaili.com/nn/{}'
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cookie': '_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJTZkN2I2MjM2NWI1Yzc3OTRjYWM4ZDhhODM0OTc2NjRjBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMS9nVkdUR2hkbTFzd29ickROUDFidS9WN1hkalFPbTBmZUhvbzJnVUtDVGc9BjsARg%3D%3D--5d66646a97cb5dbdf92cccdbd1bfe3eedc52c10f; Hm_lvt_0cf76c77469e965d2957f0553e6ecf59=1511420409; Hm_lpvt_0cf76c77469e965d2957f0553e6ecf59=1511514368',
             'Host': 'www.xicidaili.com',
-            'Proxy-Connection': 'keep-alive',
+            'Connection': 'keep-alive',
+            'If-None-Match': 'W/"39dde664bf22199337c2ad1788bb7bd5"',
             'Referer': 'http://www.xicidaili.com/nn/2',
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
         }
-        try:
-            response =  requests.get(url, headers=headers)
-            if response.status_code!=200:
-                raise Exception
-        except Exception:
-            pass
-        else:
-            response = etree.HTML(response.text)
-            ip_list = response.xpath('//table[@id="ip_list"]')
-            ip_list = ip_list[0].xpath('tr')
-            if ip_list:
-                for ip in ip_list[1:]:
-                    ipaddr = ip.xpath('td[2]/text()')[0]
-                    port = ip.xpath('td[3]/text()')[0]
-                    # item['address'] = ip.xpath('string(td[4])')[0]
-                    protocol = ip.xpath('td[6]/text()')[0].lower()
-                    proxy = '{}://{}:{}'.format(protocol, ipaddr, port)
-                    print(proxy)
-                    self.update_proxy({'proxy': proxy, 'error_count': 0})
+        for page in range(1, 4):
+            try:
+                response =  requests.get(url.format(page), headers=headers)
+                if response.status_code!=200:
+                    raise Exception
+            except Exception:
+                pass
+            else:
+                response = etree.HTML(response.text)
+                ip_list = response.xpath('//table[@id="ip_list"]')
+                ip_list = ip_list[0].xpath('tr')
+                if ip_list:
+                    for ip in ip_list[1:]:
+                        ipaddr = ip.xpath('td[2]/text()')[0]
+                        port = ip.xpath('td[3]/text()')[0]
+                        protocol = ip.xpath('td[6]/text()')[0].lower()
+                        proxy = '{}://{}:{}'.format(protocol, ipaddr, port)
+                        # proxy = {'proxy': proxy, 'error_count': 0}
+                        results.add(proxy)
+                        # self.update_proxy({'proxy': proxy, 'error_count': 0})
+        return results
+
+
+    def get_goubanjia(self):
+        """抓取goubanjia代理，有反爬措施，注意端口号"""
+        print('从goubanjia抓取代理')
+        results = set()
+        url = 'http://www.goubanjia.com/free/gngn/index{}.shtml'
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Host': 'www.goubanjia.com',
+            'Connection': 'keep-alive',
+            'Referer': 'http://www.goubanjia.com/free/gngn/index.shtml',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        }
+        for i in range(1, 4):
+            try:
+                response = requests.get(url.format(i), headers=headers)
+                if response.status_code != 200:
+                    raise Exception
+            except Exception:
+                pass
+            else:
+                response = etree.HTML(response.text)
+                ip_list = response.xpath('//table[@class="table"]/tbody/tr')
+                if ip_list:
+                    for ip in ip_list:
+                        ipaddr = ip.xpath('td[@class="ip"]/span/text()|td[@class="ip"]/div/text()')
+                        ipaddr = ''.join(ipaddr[:-1])
+                        port = ip.xpath('td[@class="ip"]/span[contains(@class, "port")]/@class')[0]
+                        port = port.split()[-1].strip()
+                        port = int(''.join((map(str, ['ABCDEFGHIZ'.index(i) for i in port]))))
+                        port = port>>3
+                        protocol = ip.xpath('td[3]/a/text()')[0]
+                        proxy = '{}://{}:{}'.format(protocol, ipaddr, port)
+                        results.add(proxy)
+                        # self.update_proxy({'proxy': proxy, 'error_count': 0, 'used_latest_time': 0})
+        return results
+
+    def get_66ip(self):
+        """抓取66ip代理"""
+        print('从66ip抓取代理')
+        results = set()
+        url = 'http://www.66ip.cn/nmtq.php'
+        pt = [0, 1]  # 0-http, 1-https
+        at = [2, 3, 4]  # 2-普通匿名，3-高级匿名，4-超级匿名
+        params = {
+            'getnum': 20,
+            'isp': 0,
+            # 'anonymoustype': at,
+            'area': 0,
+            # 'proxytype': pt,
+            'api': '66ip'
+        }
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Host': 'www.66ip.cn',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        }
+        for a in at:
+            for p in pt:
+                params['anonymoustype'] = a
+                params['proxytype'] = p
+                try:
+                    response = requests.get(url, headers=headers, params=params)
+                    if response.status_code != 200:
+                        raise Exception
+                except Exception:
+                    pass
+                else:
+                    ip_list = re.findall('(\d.*?)<br />', response.text)
+                    if ip_list:
+                        for ip in ip_list:
+                            protocol = 'http' if p==0 else 'https'
+                            proxy = '{}://{}'.format(protocol, ip)
+                            results.add(proxy)
+        return results
+
 
     def download_proxy(self):
-        """
-        开始抓取代理，代理源网站包括xici等
-        :return:
-        """
-        for i in range(1, 10):
-            url = 'http://www.xicidaili.com/nn/{}'.format(i)
-            self.get_xici(url)
+        proxies = set()
+        for target in [self.get_xici, self.get_goubanjia, self.get_66ip]:
+            proxies.update(target())
+        proxies = [{'proxy': proxy, 'error_count': 0} for proxy in proxies if proxy]
+        with Pool() as pool:
+            pool.map(self.update_proxy, proxies)
 
-    def update_proxy(self, proxy, error_count=0):
+    def update_proxy(self, proxy):
         """
         测试后更新代理，主要更新代理的出错次数
         :param proxy: 要被更新的代理,格式为{'proxy': proxy, 'error_count': 0}
         :param error_count: 代理出错次数
-        :return:
         """
-        if self._test_proxy(proxy):
+        proxy = self._test_proxy(proxy)
+        if proxy['error_count'] >= 1:
+            self._del_proxy(proxy)
+        else:
             proxy_pool.find_one_and_update(
                 {'proxy': proxy['proxy']},
                 {'$set': proxy
@@ -80,7 +167,7 @@ class ProxyPool():
 
     def _del_proxy(self, proxy):
         proxy_pool.find_one_and_delete(
-            {'proxy': proxy}
+            {'proxy': proxy['proxy']}
         )
 
     def _test_proxy(self, proxy):
@@ -96,9 +183,6 @@ class ProxyPool():
         except Exception as e:
             print(proxy['proxy'], e)
             proxy['error_count'] += 1
-            if proxy['error_count'] > 2:
-                self._del_proxy(proxy)
-                return False
         else:
             print(proxy['proxy'], '有效')
             proxy['error_count'] = 0 if proxy['error_count'] == 0 else (proxy['error_count']-1)
@@ -106,22 +190,19 @@ class ProxyPool():
         return proxy
 
     def run(self):
-        print('启动代理池服务......................................................................')
+        print('启动代理服务......................................................................')
         while True:
-            # 当代理池中有效代理代理数量<10的时候开始抓取新的代理
             proxies1 = proxy_pool.find({'error_count': {"$lt": 1}}, {'_id': 0})
-            proxies3 = proxy_pool.find({'error_count': {"$lt": 3}}, {'_id': 0})
-            print('当前pool中出错次数小于1的代理数量为：{}；出错次数小于3的代理数量为：{}'.format(proxies1.count(), proxies3.count()))
-            if proxies1.count() < 10:
+            proxy_pool.remove({'error_count': {"$gte": 1}})
+            print('当前代理池中出错次数小于1的代理数量为：{}'.format(proxies1.count()))
+            if proxies1.count() < 20:
                 self.download_proxy()
             else:
                 time.sleep(60)
 
-            # 多线程测试代理的可用性，只测试出错次数<3的代理，其余的代理认为已经失效
-            pool = Pool(4)
-            pool.map(self.update_proxy, proxies3)
-            pool.close()
-            pool.join()
+            # 多线程测试代理的可用性，只测试出错次数<1的代理，其余的代理认为已经失效
+            with Pool(4) as pool:
+                pool.map(self.update_proxy, proxies1)
 
 
 def get_proxy_available():
@@ -137,7 +218,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # get_proxy()
-    # proxy_test = ProxyTest()
-    # proxy_test.run()
-    # print(proxy)
+    # pp = ProxyPool()
+    # pp.download_proxy()
